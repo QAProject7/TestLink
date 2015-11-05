8f199bba1c15cab7ae4565aa1505a747614c8b39
yacov <jakoff@gmail.com>
2015-11-05 14:44:50 +0200

    Added new class registration test (not working until new page classes added)

---
+++ src/test/java/com/telran/RegistrationPageObjTest.java
@@ -0,0 +1,181 @@
+package com.telran;
+
+
+import com.telran.pages.RegistrationPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Oleg
+ */
+public class RegistrationPageObjTest {
+    public static String username = "User1";
+    public static String firstName = "Moshe";
+    public static String lastName = "Levi";
+    public static String email = "mosheLevi@yopmail.com";
+    public static String password = "!wTnrqiai$";
+    public static String teudat = "049919889";
+    public static String telephone = "0521234567";
+    public static String street = "Herzl";
+    public static String city = "Ashdod";
+    public static String housenumber = "33";
+
+    public WebDriver driver;
+
+    public RegistrationPage registrationPage;
+    //public DoctorMainPage mainPage;
+    // public LoginPage loginPage;
+
+    @BeforeClass(alwaysRun = true)
+    public void setup() {
+        driver = new FirefoxDriver();
+        registrationPage = PageFactory.initElements(driver, RegistrationPage.class);
+        //  mainPage = PageFactory.initElements(driver, DoctorMainPage.class);
+        //  loginPage = PageFactory.initElements(driver,LoginPage.class);
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void beforeMethodSetUp() {
+        try {
+            //    loginPage.openLoginPage(driver)
+            //           .waitUntilLoginPageIsLoaded()
+            //            .clickOnRegLink();
+//            registrationPage.openRegistrationPage(driver)
+            registrationPage.waitUntilRegPageIsLoaded();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test(groups = {"smoke", "positive"})
+    public void LoginSuccess() {
+        try {
+            registrationPage
+                    .fillUsernameField(username)
+                    .fillFirstNameField(firstName)
+                    .fillPasswordField(password)
+                    .fillEmailField()
+                    .fillLastNameField(lastName)
+                    .fillConfPasswordField(password)
+                    .fillIdField()
+                    .fillMobile(telephone)
+                    .fillStreetField(street)
+                    .fillHouseField(housenumber)
+                    .fillCityField(city)
+                    .clickOnSubmitButton();
+
+            //    mainPage.waitUntilMainPageIsLoaded();
+            //    Assert.assertTrue(mainPage.isOnMainPage(), "The Main Page doesn't open");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test(groups = {"smoke", "negative"})
+    public void LoginWithoutFirstName() {
+        try {
+            registrationPage
+                    .fillUsernameField(username)
+                    .fillFirstNameField("")
+                    .fillPasswordField(password)
+                    .fillEmailField()
+                    .fillLastNameField(lastName)
+                    .fillConfPasswordField(password)
+                    .fillIdField()
+                    .fillStreetField(street)
+                    .fillHouseField(housenumber)
+                    .fillCityField(city)
+                    .clickOnSubmitButton();
+
+            Assert.assertTrue(registrationPage.isOnRegistrationPage(), "The Main Page is opened");
+            Assert.assertTrue(registrationPage.alertMessageNotValidFirsrName(), "Alert message 'שם פרטי חובה' is not presented");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @AfterTest(alwaysRun = true)
+    public void tearDown() {
+        // this.driver.quit();
+    }
+
+    /*@Test(groups = {"smoke", "negative"})
+    public void LoginWithoutAtInUserNameField() {
+      //  Log.info("Checking inability lodin without @ in email field");
+        try {
+            loginPage
+                    .fillEmailField("")
+                    .fillPasswordField(PASSWORD)
+                    .waitUntilAllertEmailIsLogIsLoaded()
+                    .clickOnLogin();
+            assertTrue("The Email is valid", loginPage.alertMessageInvalidEmail());
+            assertTrue("The current page is changed", loginPage.isOnLoginPage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Reporter.log("Not logged in successful");
+    }
+    @Test(groups = {"smoke", "negative"})
+    public void LoginWithPasswordContains1Symbol() {
+        Log.info("Checking inability lodin with password contains 1 symbol");
+        try {
+            loginPage
+                    .fillEmailField(USER)
+                    .fillPasswordField("1")
+                    .waitUntilAllertPasswordIsLogIsLoaded()
+                    .clickOnLogin();
+            assertTrue("The Password is valid", loginPage.alertMessageInvalidPassword());
+            assertTrue("The current page is changed", loginPage.isOnLoginPage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Reporter.log("Not logged in successful");
+    }
+
+    @Test(groups = {"smoke", "positive"})
+    public void ForgotPassword() {
+        Log.info("Checking ability recreate password");
+        try {
+            loginPage
+                    .clickOnForgotPasswordLink();
+            resetYourPasswordPage.waitUntilResetPageIsLoaded();
+            assertTrue("The Reset Password Page doesn't open", resetYourPasswordPage.isOnResetPage());
+            resetYourPasswordPage.fillEmailField(USER);
+            resetYourPasswordPage.clickOnEmailMe();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Reporter.log("Password recreated successful");
+    }
+
+    @Test(groups = {"smoke", "negative"})
+    public void LoginWithEmptyFields() {
+        Log.info("Checking inability lodin with empty fields");
+        try {
+            loginPage
+                    .fillEmailField("")
+                    .fillPasswordField("")
+                    .waitUntilAllertEmailIsLogIsLoaded()
+                    .clickOnLogin();
+            assertTrue("The Email is valid", loginPage.alertMessageInvalidEmail());
+            assertTrue("The Password is valid", loginPage.alertMessageInvalidPassword());
+            assertTrue("The current page is changed", loginPage.isOnLoginPage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Reporter.log("Not logged in successful");
+    }*/
+
+
+}
