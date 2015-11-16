3fc617b3f45b3c3c249efdead41ceff53641e737
Irina <irina.okina@yandex.ru>
2015-11-16 10:30:22 +0200

    DoctorsPage

--- src/test/java/com/telran/pages/DoctorsPage.java
+++ src/test/java/com/telran/pages/DoctorsPage.java
@@ -1,6 +1,5 @@
 package com.telran.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -80,13 +79,7 @@ public class DoctorsPage extends Page {
 
     }
 
-    public void openPatientPage(String teudat) {
-        String locator = "//td[contains(text(),'" + teudat + "')]/..//*[@class='LinkBtnPatients GreenBtn']";
-        WebElement greenbutton = driver.findElement(By.xpath(locator));
-        greenbutton.click();
 
-        //td[contains(text(),'1970524')]/..//*[@class='LinkBtnPatients GreenBtn']
-    }
 
     public boolean isOnMainPage() {
         return exists(namesField);
