b183403a53bcc0a50f175cc216c39ca27ab6850e
oflops <oflops@gmail.com>
2015-11-23 10:12:41 +0200

    new class by Olga

--- src/test/java/com/telran/СhangePassRegPatientOlgaTest.java
+++ src/test/java/com/telran/СhangePassRegPatientOlgaTest.java
@@ -14,7 +14,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
-public class GhengePassRegPatientOlgaTest {
+public class СhangePassRegPatientOlgaTest {
     public static String firstName = "Moshe";
     public static String lastName = "Levi";
     public static String email = "";
