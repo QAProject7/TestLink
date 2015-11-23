cf6925e7ae9021abba94e37ad2d95ee0a3034724
yacov <jakoff@gmail.com>
2015-11-23 16:09:56 +0200

    correcting CreatePatien test and page classes

--- src/test/java/com/telran/CreateNewPatientTest.java
+++ src/test/java/com/telran/CreateNewPatientTest.java
@@ -4,32 +4,31 @@ import com.telran.pages.CreateNewPatientPage;
 import com.telran.pages.DoctorsPage;
 import com.telran.pages.LoginMaksimPage;
 import org.apache.log4j.Logger;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Reporter;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-public class CreateNewPatientTest {
+public class CreateNewPatientTest extends TestNgTestBase {
     public static String username = "1298Doctor";
     public static String password = "LinkCare!!11";
     public static String zeut;
     public static String email;
     public static String emailTeacher;
+    public static String birthDate = "21/10/2013";
+    public static String meetingDate;
     private static Logger Log = Logger.getLogger(LogLog4j.class.getName());
     public LoginMaksimPage loginPage;
     public DoctorsPage doctorsPage;
     public CreateNewPatientPage createNewPatientpage;
-    private WebDriver driver;
+    // private WebDriver driver;
 
     @BeforeClass(alwaysRun = true)
     public void setup() throws InterruptedException {
-        driver = new FirefoxDriver();
+        // driver = new FirefoxDriver();
         //registrationPage = PageFactory.initElements(driver, RegistrationPage.class);
         //  mainPage = PageFactory.initElements(driver, DoctorMainPage.class);
         loginPage = PageFactory.initElements(driver, LoginMaksimPage.class);
@@ -38,12 +37,14 @@ public class CreateNewPatientTest {
         zeut = createNewPatientpage.generateZeut();
         email = createNewPatientpage.generateParentEmail();
         emailTeacher = createNewPatientpage.generateTeacherEmail();
+        meetingDate = createNewPatientpage.createMeetingDate();
         loginPage.
                 openLoginPage(driver)
                 .fillUsernameField(username)
                 .fillPasswordField(password)
                 .clickOnLoginButton();
         doctorsPage.waitUntilMainPageIsLoaded();
+        driver.manage().window().maximize();
         doctorsPage.clickOnAddPatient();
 
     }
@@ -58,6 +59,22 @@ public class CreateNewPatientTest {
         try {
             Log.info("Create new patient started");
             createNewPatientpage.createPatientOneParent(zeut, email);
+            createNewPatientpage.waitUntilPageIsLoaded();
+            createNewPatientpage.fillFirstNameField("PatientChildFirst")
+                    .fillLastNamefield("PatientChildLast")
+                    .fillZeutfield(zeut)
+                    .fillWeightfield("2")
+                    .filltEmailField(email)
+                    .sendFirstEmail()
+                    .addTeacher()
+                    .filltEmailField(emailTeacher)
+                    .sendFirstEmail();
+            Thread.sleep(4000);
+            createNewPatientpage.fillMeetingDateAndTime(meetingDate)
+                    .fillBirthDayfield(birthDate)
+                    .clickSaveAccount();
+            Thread.sleep(4000);
+            doctorsPage.waitUntilMainPageIsLoaded();
             doctorsPage.isPatientExists(zeut);
             Reporter.log("new Patient added");
         } catch (IOException e) {
@@ -80,8 +97,8 @@ public class CreateNewPatientTest {
         }
     }
 
-    @AfterClass(alwaysRun = true)
+  /* @AfterClass(alwaysRun = true)
     public void tearDown() {
         this.driver.quit();
-    }
+    }*/
 }
