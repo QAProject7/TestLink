7caec8d341fcdbbf40c5df35864839cdd7697ff0
Leonid Petrunin <Leonid Petrunin>
2015-12-02 17:44:50 +0200

    update QuestionnaireChronicDiseasesPatientPetruninTest.java

--- src/test/java/com/telran/pages/DoctorQuestionnaireChronicDiseasesPetruninPage.java
+++ src/test/java/com/telran/pages/DoctorQuestionnaireChronicDiseasesPetruninPage.java
@@ -2,9 +2,12 @@ package com.telran.pages;
 
 import com.telran.LogLog4j;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 /**
  * Created by leoni on 02.12.2015.
@@ -31,3 +34,49 @@ public class DoctorQuestionnaireChronicDiseasesPetruninPage extends Page {
 
 
 }
+   /* public QuestionnaireChronicDiseasesPatientPetruninPage clickTableButtom() {
+        clickElement(clickTableButtom);
+        Log.info("TableButtom was pushed");
+        return this;
+    }
+
+    public boolean isOnframe() {
+        return exists(onFrame);
+    }
+
+    public QuestionnaireChronicDiseasesPatientPetruninPage fillElements() {
+        List<WebElement> sections = form1.findElements(By.tagName("tr"));
+
+        Log.info("Find all table elements " + sections.size());
+        List<WebElement> spanWithInput, radioButtons;
+        for (WebElement sec : sections) {
+            spanWithInput = sec.findElements(By.tagName("td"));
+            int rndValue = (int) (Math.random() * 3);
+            if (spanWithInput.size() == 5) {
+                WebElement currentSpan = spanWithInput.get(rndValue);
+                radioButtons = currentSpan.findElements(By.tagName("input"));
+                for (WebElement but : radioButtons) {
+                    if(Integer.parseInt(but.getAttribute("value"))==rndValue){
+                        but.click();
+                    }
+                }
+            } else {
+                //Дописать метод поиска пустых строк, внесения их в HashMap и заполнения их
+            }
+        }
+        fillField("test");
+        Log.info("Text was write into the last TextField");
+        return this;
+    }
+
+    public QuestionnaireChronicDiseasesPatientPetruninPage fillField(String text) {
+        setElementText(textIntroduce, text);
+        return this;
+    }
+
+    public QuestionnaireChronicDiseasesPatientPetruninPage clickSubmitButtom() {
+        clickElement(submitButtom);
+        Log.info("SubmitButtom was pushed");
+        return this;
+    }
+}*/
